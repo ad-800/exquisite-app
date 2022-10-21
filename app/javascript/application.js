4e6d90a2f8e4fe1e836bc790a513d5d23568dbd4
Adalyne Cummins <adalynecummins@gmail.com>
2022-10-21 09:43:57 -0400

    actioncable console.log

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,27 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+ActionCable = require('actioncable')
+
+var cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+
+function establishActionCableConnection() {
+    cable.subscriptions.create('GameRoomChannel', {
+      connected() {
+       console.log('connected to the room')
+      },
+
+      disconnected() {
+
+      },
+
+      received(data) {
+       console.log(data)
+
+      },
+
+    });
+  }
+
+	establishActionCableConnection()
